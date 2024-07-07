e1526c20fb8c83f619f1356b03f0cc100e1b8388
BrunoSerpa <serpabruno2005@gmail.com>
2024-07-07 11:47:05 -0300

    Melhorei as funções de formatação

--- Sistema/formatacao.py
+++ Sistema/formatacao.py
@@ -1,25 +1,25 @@
-def formatacaoEndereco(arquivoJson, comCodigo=False):
-    if comCodigo: print(f'ID: {arquivoJson.id}')
-    print(f'{arquivoJson.rua}, {arquivoJson.numero} ({arquivoJson.descricao}) - {arquivoJson.bairro}')
-    print(f'CEP: {formatar_cep(arquivoJson.cep)}')
-    print(f'{arquivoJson.cidade} - {arquivoJson.estado} ({arquivoJson.pais})')
+def formatacaoEndereco(endereco, comCodigo=False):
+    if comCodigo: print(f'ID: {endereco.id}')
+    print(f'{endereco.rua}, {endereco.numero} ({endereco.descricao}) - {endereco.bairro}')
+    print(f'CEP: {formatarCep(endereco.cep)}')
+    print(f'{endereco.cidade} - {endereco.estado} ({endereco.pais})')
 
 
-def formatacaoProduto(arquivoJson, vendedor=None, comCodigo=False):
-    if comCodigo: print(f'ID: {arquivoJson.id}')
-    print(f'Produto: {arquivoJson.nome_produto}')
-    print(f'Valor: R${arquivoJson.valor_produto:.2f}')
+def formatacaoProduto(produto, vendedor=None, comCodigo=False):
+    if comCodigo: print(f'ID: {produto.id}')
+    print(f'Produto: {produto.nome_produto}')
+    print(f'Valor: R${produto.valor_produto:.2f}')
     if vendedor:
         print("-----------------------------------------------")
         print(f"Vendedor: {vendedor.nome_vendedor}")
-        print(f"CNPJ: {formatar_cnpj(vendedor.cnpj)}")
+        print(f"CNPJ: {formatarCnpj(vendedor.cnpj)}")
         print("-----------------------------------------------")
     
-def formatacaoVendedor(arquivoJson, enderecos=[], produtos=[], compras=[], comCodigo=False):
-    if comCodigo: print(f"ID: {arquivoJson.id}")
-    print(f"Nome: {arquivoJson.nome_vendedor}")
-    print(f"CNPJ: {formatar_cnpj(arquivoJson.cnpj)}")
-    print(f"Telefone: {formatar_telefone(arquivoJson.telefone_vendedor)}")
+def formatacaoVendedor(vendedor, enderecos=[], produtos=[], compras=[], comCodigo=False):
+    if comCodigo: print(f"ID: {vendedor.id}")
+    print(f"Nome: {vendedor.nome_vendedor}")
+    print(f"CNPJ: {formatarCnpj(vendedor.cnpj)}")
+    print(f"Telefone: {formatarTelefone(vendedor.telefone_vendedor)}")
     for endereco in enderecos:
         print("-----------------------------------------------")
         formatacaoEndereco(endereco)
@@ -30,19 +30,18 @@ def formatacaoVendedor(arquivoJson, enderecos=[], produtos=[], compras=[], comCo
         print("-----------------------------------------------")
         formatacaoCompras(
             compra,
-            produtos=compra.produtos,
             cliente=compra.cliente,
             enderecoCliente=compra.enderecoCliente,
-            vendedor=compra.vendedor,
-            enderecoVendedor=compra.enderecoVendedor
+            enderecoVendedor=compra.enderecoVendedor,
+            produtos=compra.produtos,
         )
-    if len(enderecos)>0 or len(produtos)>0 or len(compras)>0: print("-----------------------------------------------")
+    if len(enderecos) > 0 or len(produtos) > 0 or len(compras) > 0: print("-----------------------------------------------")
 
-def formatacaoUsuario(arquivoJson, enderecos=[], favoritos=[], compras=[], comCodigo=False):
-    if comCodigo: print(f"ID: {arquivoJson.id}")
-    print(f"Nome: {arquivoJson.nome_usuario}")
-    print(f"CPF: {formatar_cpf(arquivoJson.cpf)}")
-    print(f"Telefone: {formatar_telefone(arquivoJson.telefone_usuario)}")
+def formatacaoUsuario(usuario, enderecos=[], favoritos=[], compras=[], comCodigo=False):
+    if comCodigo: print(f"ID: {usuario.id}")
+    print(f"Nome: {usuario.nome_usuario}")
+    print(f"CPF: {formatarCpf(usuario.cpf)}")
+    print(f"Telefone: {formatarTelefone(usuario.telefone_usuario)}")
     for endereco in enderecos:
         print("-----------------------------------------------")
         formatacaoEndereco(endereco)
@@ -56,22 +55,21 @@ def formatacaoUsuario(arquivoJson, enderecos=[], favoritos=[], compras=[], comCo
         print("-----------------------------------------------")
         formatacaoCompras(
             compra,
-            produtos=compra.produtos,
-            cliente=compra.cliente,
             enderecoCliente=compra.enderecoCliente,
             vendedor=compra.vendedor,
-            enderecoVendedor=compra.enderecoVendedor
+            enderecoVendedor=compra.enderecoVendedor,
+            produtos=compra.produtos,
         )
-    if len(enderecos)>0 or len(favoritos)>0 or len(compras)>0: print("-----------------------------------------------")
+    if len(enderecos) > 0 or len(favoritos) > 0 or len(compras) > 0: print("-----------------------------------------------")
 
-def formatacaoCompras(arquivoJson, cliente=None, enderecoCliente=None, vendedor=None, enderecoVendedor=None, produtos=[], comCodigo=False):
-    print(f'Data da Compra: {arquivoJson.data_compra}')
+def formatacaoCompras(compra, cliente=None, enderecoCliente=None, vendedor=None, enderecoVendedor=None, produtos=[], comCodigo=False):
+    print(f'Data da Compra: {compra.data_compra}')
     if cliente:
         print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
         print("Cliente:")
         if comCodigo: print(f"ID: {cliente.id}")
         print(f"Nome: {cliente.nome_usuario}")
-        print(f"CPF: {formatar_cpf(cliente.cpf)}")
+        print(f"CPF: {formatarCpf(cliente.cpf)}")
         if enderecoCliente:
             print("Endereço:")
             print("-----------------------------------------------")
@@ -83,7 +81,7 @@ def formatacaoCompras(arquivoJson, cliente=None, enderecoCliente=None, vendedor=
         print("Vendedor:")
         if comCodigo: print(f"ID: {vendedor.id}")
         print(f"Nome: {vendedor.nome_vendedor}")
-        print(f"CNPJ: {formatar_cnpj(vendedor.cnpj)}")
+        print(f"CNPJ: {formatarCnpj(vendedor.cnpj)}")
         if enderecoVendedor:
             print("Endereço:")
             print("-----------------------------------------------")
@@ -97,23 +95,23 @@ def formatacaoCompras(arquivoJson, cliente=None, enderecoCliente=None, vendedor=
         formatacaoProduto(produto)
     print("-----------------------------------------------")
     print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-    print("Total:", arquivoJson.valor_total)
+    print("Total:", compra.valor_total)
 
-def formatar_telefone(telefone):
+def formatarTelefone(telefone):
     if len(telefone) == 8: return f'{telefone[0:4]}-{telefone[4:8]}'
     elif len(telefone) == 9: return f'{telefone[0:5]}-{telefone[5:9]}'
     elif len(telefone) == 10: return f'({telefone[0:2]}) {telefone[2:6]}-{telefone[6:10]}'
     elif len(telefone) == 11: return f'({telefone[0:2]}) {telefone[2:7]}-{telefone[7:11]}'
     else: return telefone
 
-def formatar_cpf(cpf):
+def formatarCpf(cpf):
     if len(cpf) == 11: return f'{cpf[0:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}'
     else: return cpf
 
-def formatar_cnpj(cnpj):
+def formatarCnpj(cnpj):
     if len(cnpj) == 14: return f'{cnpj[0:2]}.{cnpj[2:5]}.{cnpj[5:8]}/{cnpj[8:12]}-{cnpj[12:14]}'
     else: return cnpj
 
-def formatar_cep(cep):
+def formatarCep(cep):
     if len(cep) == 8: return f'{cep[0:5]}-{cep[5:8]}'
     else: return cep
